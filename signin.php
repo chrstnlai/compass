0402fda9dbeba205a9185d8a55c837c82dd04cc4
senahkim <senahkim@usc.edu>
2024-12-05 16:03:33 -0800

    Update signin.php

--- signin.php
+++ signin.php
@@ -347,13 +347,8 @@ if ($mysql->connect_errno) {
     </style>
 </head>
 <body>
-    <div class="navbar">
-        <div class="logo">
-            <img src="images/CompassTransparent.png" alt="logoimage">
-            <h2>COMPASS</h2>
-        </div>
-        <img src="images/Login.png" alt="login" class="login-icon">
-    </div>
+
+<?php include 'nav.php'; ?>
 
     <div class="main-container">
         <h2>Sign In</h2>
