eae96a557ac4d2e0c6388c56431c17e0f0fbedf1
apchang <apchang@usc.edu>
2024-12-06 21:05:46 -0800

    Revert "Merge branch 'main' of https://github.com/chrstnlai/compass"
    This reverts commit b41832d305855115dc65c452360a88ea8513ebb0, reversing
    changes made to 7b4ed1cd291e361730c8afbee1e2423314eb1afd.

--- search.php
+++ search.php
@@ -20,7 +20,7 @@ if($mysql->connect_errno) {
 
 <html>
 <head>
-    <meta name="viewport" charset = "UTF-8" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Krona+One&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
@@ -204,6 +204,7 @@ if($mysql->connect_errno) {
         margin: 0;
         padding: 0 0 50px 0;
     }
+
 
     nav .title {
         text-align: center;
